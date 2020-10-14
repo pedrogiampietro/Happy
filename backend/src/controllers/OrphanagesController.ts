7424b88cb3269bca61fc8e10d1683570635a06fe
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-10-14 12:17:57 +0900

    Added TypeORM, sqlite, models, images migrations.

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanage'
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage)
+    const orphanages = await orphanagesRepository.find()
+
+    return response.json(orphanages)
+  },
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+    const orphanagesRepository = getRepository(Orphanage)
+    const orphanage = await orphanagesRepository.findOneOrFail(id)
+
+    return response.json(orphanage)
+  },
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanages = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    })
+
+    await orphanagesRepository.save(orphanages)
+
+    return response.status(201).json(orphanages)
+  },
+}
